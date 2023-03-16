3d1569ccf90c222b7b6709bac0f53045e355accf
unityclases <126904973+unityclases@users.noreply.github.com>
2023-03-16 17:45:23 +0100

    Update Platform2.cs

--- Platform2.cs
+++ Platform2.cs
@@ -17,7 +17,7 @@ public class Platform : MonoBehaviour
             transform.position += direction * speed * Time.deltaTime;
         }
 
-        if (gameObject.transform.position.y <= StopPoint1.position.y || gameObject.transform.position.y >= StopPoint2.position.y) //Cambiar los .z por la direccion que quieras
+        if (gameObject.transform.position.z <= StopPoint1.position.z || gameObject.transform.position.z >= StopPoint2.position.z) 
         {
             direction *= -1;
         }
