a3798e99a28dadc018678834351612cf475cc773
unityclases <126904973+unityclases@users.noreply.github.com>
2023-05-09 16:27:27 +0200

    Create WizardMotion.cs

---
+++ WizardMotion.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class WizardMotion : MonoBehaviour
+{
+    public Animator animator;
+ 
+    void Update()
+    {
+        animator.SetFloat("velX", Input.GetAxis("Horizontal"), 0.1f, Time.deltaTime);
+        animator.SetFloat("velY", Input.GetAxis("Vertical"), 0.1f, Time.deltaTime);
+    }
+}
